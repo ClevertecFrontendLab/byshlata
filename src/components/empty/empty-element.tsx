becf22f0b17c496c03a0dce5d087f4ba00d9a246
Ivan Linkevich <79784960+Ivan6813@users.noreply.github.com>
2024-03-01 12:59:59 +0300

    Update empty-element.tsx

--- src/components/empty/empty-element.tsx
+++ src/components/empty/empty-element.tsx
@@ -1,11 +1,9 @@
-// @ts-nocheck
 import Empty from 'antd/es/empty/empty';
 
 import styles from './empty-element.module.css';
 
 export const EmptyElement = () => (
     <div className={styles.empty}>
-        // @ts-ignore
         <Empty image='https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg' />
     </div>
 );
